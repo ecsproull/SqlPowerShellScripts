1b77147ed20c3b03b1d0d66bc8aec0ac964bd28e
ecsproull <ecs3@po.cwru.edu>
2026-06-12 10:33:01 -0700

    Three backup locations now

--- FullBackup.ps1
+++ FullBackup.ps1
@@ -1,12 +1,28 @@
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
 
-$file = "\\wc_server\LocalSqlBackup\Accounting_live\Full\Accounting_live_FULL_$timestamp.bak"
+$localPath = "C:\LocalSqlBackup\Accounting_live\Full"
+$localFile = "$localPath\Accounting_live_FULL_$timestamp.bak"
+
+# Make sure local folder exists
+New-Item -ItemType Directory -Path $localPath -Force | Out-Null
 
 $sql = @"
 BACKUP DATABASE Accounting_live
-TO DISK = '$file'
+TO DISK = '$localFile'
 WITH CHECKSUM;
 "@
 
 Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" `
               -Query $sql
+
+# Copy to machine B
+robocopy `
+    "C:\LocalSqlBackup\Accounting_live\Full" `
+    "D:\LocalSqlBackup\Accounting_live\Full" `
+    *.bak /XO /R:3 /W:5
+
+# Copy to wc_server
+robocopy `
+    "C:\LocalSqlBackup\Accounting_live\Full" `
+    "\\wc_server\LocalSqlBackup\Accounting_live\Full" `
+    *.bak /XO /R:3 /W:5
--- LogBackup.ps1
+++ LogBackup.ps1
@@ -1,12 +1,28 @@
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
 
-$file = "\\wc_server\LocalSqlBackup\Accounting_live\Logs\Accounting_live_LOG_$timestamp.trn"
+$localPath = "C:\LocalSqlBackup\Accounting_live\Logs"
+$localFile = "$localPath\Accounting_live_LOG_$timestamp.trn"
+
+# Make sure local folder exists
+New-Item -ItemType Directory -Path $localPath -Force | Out-Null
 
 $sql = @"
 BACKUP LOG Accounting_live
-TO DISK = '$file'
+TO DISK = '$localFile'
 WITH CHECKSUM;
 "@
 
 Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" `
               -Query $sql
+
+# Copy to external drive
+robocopy `
+    "C:\LocalSqlBackup\Accounting_live\Logs" `
+    "D:\LocalSqlBackup\Accounting_live\Logs" `
+    *.trn /XO /R:3 /W:5
+
+# Copy to wc_server
+robocopy `
+    "C:\LocalSqlBackup\Accounting_live\Logs" `
+    "\\wc_server\LocalSqlBackup\Accounting_live\Logs" `
+    *.trn /XO /R:3 /W:5
